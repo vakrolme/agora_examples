d74ae8d0c63d1d7ab8c94141a2e18189abf2e002
vakrolme <vakrolme@gmail.com>
2022-10-10 12:34:17 +0100

    fix

--- lib/config/agora.config.dart
+++ lib/config/agora.config.dart
@@ -1,13 +1,13 @@
 /// Get your own App ID at https://dashboard.agora.io/
 String get appId {
   // Allow pass an `appId` as an environment variable with name `TEST_APP_ID` by using --dart-define
-  return const String.fromEnvironment('TEST_APP_ID', defaultValue: APP_ID);
+  return const String.fromEnvironment('TEST_APP_ID', defaultValue: 'APP_ID');
 }
 
 /// Please refer to https://docs.agora.io/en/Agora%20Platform/token
 String get token {
   // Allow pass a `token` as an environment variable with name `TEST_TOKEN` by using --dart-define
-  return const String.fromEnvironment('TEST_TOKEN', defaultValue: TOKEN);
+  return const String.fromEnvironment('TEST_TOKEN', defaultValue: 'TOKEN');
 }
 
 /// Your channel ID
@@ -15,7 +15,7 @@ String get channelId {
   // Allow pass a `channelId` as an environment variable with name `TEST_CHANNEL_ID` by using --dart-define
   return const String.fromEnvironment(
     'TEST_CHANNEL_ID',
-    defaultValue: TEST,
+    defaultValue: 'TEST',
   );
 }
 
